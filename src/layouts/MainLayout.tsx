0ab76d16f5511c26facaa960f6b0a43c8f99131b
Stephen Tersoo <stephentersoo17@gmail.com>
2025-12-05 13:44:27 +0100

    Fix error

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 const MainLayout: React.FC = () => {
